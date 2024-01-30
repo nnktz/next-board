5bb5a4404a67ddf031e0c1062e39f392854f3a44
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2024-01-30 14:35:56 +0700

    dashboard layout setups

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export const OrgSidebar = () => {
+  return (
+    <div className="hidden w-[206px] flex-col space-y-6 bg-red-500 pl-5 pt-5 lg:flex">
+      OrgSidebar
+    </div>
+  )
+}
